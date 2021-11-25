e161dcffd74bd17ac4660682f2b45ed1314876ed
Jason Jiang <jasonjiang316@gmail.com>
2021-11-24 21:37:55 -0500

    .env

--- server.js
+++ server.js
@@ -7,12 +7,10 @@ dotenv.config();
 
 var ejs = require('ejs');
 const PORT = process.env.PORT;
-const user = process.env.DB_USER;
-const pass = process.env.DB_PASS;
 
 const { MongoClient } = require('mongodb');
 
-const uri = `mongodb+srv://${user}:${pass}@cluster0.yukz6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = process.env.URL;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
