afb866a7dc355a4720cf7de5974a27d19341a631
Jason Jiang <jasonjiang316@gmail.com>
2021-11-25 01:14:28 -0500

    created database and signup form

--- server.js
+++ server.js
@@ -1,21 +1,42 @@
 const express = require('express');
 var path = require('path');
+const mongoose = require('mongoose');
 const app = express();
+const bodyParser = require('body-parser');
 
 const dotenv = require('dotenv');
 dotenv.config();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 var ejs = require('ejs');
 const PORT = process.env.PORT || 3000;
 
 const { MongoClient } = require('mongodb');
-
-const uri = "mongodb+srv://Jason:Totojiang@cluster0.yukz6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+let db;
+const uri = "mongodb+srv://Jason:Totojiang@cluster0.yukz6.mongodb.net/chalkboard?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+    if (!err) {
+        db = client.db("chalkboard");
+        console.log('MongoDB Connection Succeeded.');
+    } 
+    else {
+        console.log('Error in DB connection : ' + err);
+    }
+    // perform actions on the collection object
+});
+
+mongoose.connect('mongodb+srv://Jason:Totojiang@cluster0.yukz6.mongodb.net/chalkboard?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, (err) => {
+  if (!err) {
+    console.log('MongoDB Connection Succeeded.');
+  } else {
+    console.log('Error in DB connection : ' + err);
+  }
 });
 
 app.set('view engine', 'ejs');
@@ -24,7 +45,7 @@ app.use(express.static(__dirname + '/src'));
 app.set('views', path.join(__dirname, 'src/views'))
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {valid: ""});
 });
 
 app.get('/student_homepage', (req, res) => {
@@ -40,7 +61,7 @@ app.get('/login_admin', (req, res) => {
 })
 
 app.get('/sign_up', (req, res) => {
-    res.render('signup');
+    res.render('signup', {valid: ""});
 })
 
 app.get('/professor_homepage', (req, res) => {
@@ -78,3 +99,91 @@ app.get('/student_lecture', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Express listening on port ${PORT}`);
 })
+
+var Schema = mongoose.Schema;
+
+userSchema = new Schema({
+  unique_id: Number,
+  email: {
+    type: String,
+    required: true,
+    format: "Email",
+  },
+  first: {
+    type: String,
+    required: true,
+  },
+  last: {
+    type: String,
+    required: true,
+  },
+  userType: {
+    type: String,
+    require: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  confirmPassword: {
+    type: String
+  },
+  courses: {
+    type: [String] 
+  }
+});
+
+User = mongoose.model('User', userSchema);
+
+app.use(function(req, res, next) {
+    if (!req.user)
+        res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    next();
+});
+
+app.post('/sign_up', function (req, res, next) {
+  console.log(req.body);
+  var personInfo = req.body;
+
+  if (!personInfo.email || !personInfo.first || !personInfo.last || !personInfo.password || !personInfo.confirmPassword) {
+    return res.render("signup", { valid: "Fill all the required fields!" });
+  }
+  else {
+    if (personInfo.password == personInfo.confirmPassword) {
+      User.findOne({ email: personInfo.email }, function (err, data) {
+        if (!data) {
+          var c;
+          User.findOne({}, function (err, data) {
+            if (data) {
+              console.log("if");
+              c = data.unique_id + 1;
+            } else {
+              c = 1;
+            }
+            var newPerson = new User({
+              unique_id: c,
+              email: personInfo.email,
+              first: personInfo.first,
+              last: personInfo.last,
+              userType: personInfo.userType,
+              password: personInfo.password,
+              confirmPassword: personInfo.confirmPassword,
+              courses: [""]
+            });
+            newPerson.save(function (err, Person) {
+              if (err)
+                console.log(err);
+              else
+                console.log('Success');
+            });
+          }).sort({ _id: -1 }).limit(1);
+          return res.render("index", { valid: "Sucess!" });
+        } else {
+          return res.render("signup", { valid: "Email is already in use!" });
+        }
+      });
+    } else {
+      return res.render(__dirname + "signup", { valid: "Password not matched!" });
+    }
+  }
+});
