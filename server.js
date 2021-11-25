72bd16e6848212d5442f8f2e547f41ba62acf2c8
Jason Jiang <jasonjiang316@gmail.com>
2021-11-24 22:37:52 -0500

    removed .env for now

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 var ejs = require('ejs');
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 const { MongoClient } = require('mongodb');
 
-const uri = process.env.URL;
+const uri = "mongodb+srv://Jason:Totojiang@cluster0.yukz6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
